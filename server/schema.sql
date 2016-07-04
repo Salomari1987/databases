a2e43de297631d7d9ef1ed25361b26422e394826
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-04 22:08:33 +0400

    Getting started

--- server/schema.sql
+++ server/schema.sql
@@ -3,11 +3,23 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+	id int (3) auto-increment,
+	innertext varchar (255),
+	dateof varchar (100),
+	username varchar (100),
+	roomname varchar (100)
+);
+CREATE TABLE users (
+	id int(3) auto-increment,
+	name varchar (100),
+	friends?? //self join
 );
 
+CREATE TABLE rooms (
+	id
+	name
+)
 /* Create other tables and define schemas for them here! */
-
 
 
 
